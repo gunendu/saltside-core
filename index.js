b745eb1b2b5adf378b9c3dc28ddfc9914bfab47e
gunendu <gunendu@gmail.com>
2016-09-15 02:23:15 +0530

    removing unnecessary lib files

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ SdApi.Controllers = {
     BirdController: require('./controllers/BirdsController'),
 };
 
-SdApi.Errors = {
-    Base: require('./lib/errors/base'),
-    Object: require('./lib/errors/object'),
-    User: require('./lib/errors/user'),
-    Api: require('./lib/errors/api'),
-};
-
 SdApi.Logger = require('./lib/logging');
 
 module.exports = SdApi;
